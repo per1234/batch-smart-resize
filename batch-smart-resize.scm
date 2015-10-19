5cd2709441e31fc86555b2cb5205db2eb9fcdb61
per1234 <per1234@users.noreply.github.com>
2015-10-19 05:57:44 -0700

    Add GIF output type

--- batch-smart-resize.scm
+++ batch-smart-resize.scm
@@ -58,47 +58,70 @@
           (outputFilenameNoExtension (string-append (string-append destinationPath "/") (unbreakupstr (reverse (cdr (reverse (strbreakup (car (reverse (strbreakup filename "\\"))) ".")))) ".")))  ;strip the extension(from http://stackoverflow.com/questions/1386293/how-to-parse-out-base-file-name-using-script-fu) - this probably only works on windows
         )
         ;save file
-        (if (= outputType 1)
-          (let*
-            (
-              (outputFilename (string-append outputFilenameNoExtension ".png"))  ;add the new extension
+        (cond
+          ((= outputType 0)
+            (let*
+              (
+                (outputFilename (string-append outputFilenameNoExtension ".png"))  ;add the new extension
+              )
+              ;file-png-save parameters
+                ;The run mode { RUN-INTERACTIVE (0), RUN-NONINTERACTIVE (1) }
+                ;Input image
+                ;Drawable to save
+                ;filename
+                ;raw-filename - this doesn't appear to do anything
+                ;Adam7 interlacing(Interlacing) Checking interlace allows an image on a web page to be progressively displayed as it is downloaded. Progressive image display is useful with slow connection speeds, because you can stop an image that is of no interest; interlace is of less use today with our faster connection speeds.
+                ;deflate compression factor (0-9) Since compression is not lossy, the only reason to use a compression level less than 9, is if it takes too long to compress a file on a slow computer. Nothing to fear from decompression: it is as quick whatever the compression level.
+                ;Write bKGD chunk(save background color) If your image has many transparency levels, the Internet browsers that recognize only two levels, will use the background color of your Toolbox instead. Internet Explorer up to version 6 did not use this information.
+                ;Write gAMMA chunk(Save gamma) Gamma correction is the ability to correct for differences in how computers interpret color values. This saves gamma information in the PNG that reflects the current Gamma factor for your display. Viewers on other computers can then compensate to ensure that the image is not too dark or too bright.
+                ;Write oFFs chunk(Save layer offset) PNG supports an offset value called the “oFFs chunk”, which provides position data. Unfortunately, PNG offset support in GIMP is broken, or at least is not compatible with other applications, and has been for a long time. Do not enable offsets, let GIMP flatten the layers before saving, and you will have no problems.
+                ;Write pHYS chunk(Save Resolution) Save the image resolution, in ppi (pixels per inch). Are the pHYS and tIME parameters swapped in DB Browser?(from http://beefchunk.com/documentation/lang/gimp/GIMP-Scripts-Fu.html)
+                ;Write tIME chunk(Save Creation Time) Date the file was saved.
+              (file-png-save RUN-NONINTERACTIVE image (car (gimp-image-get-active-drawable image)) outputFilename outputFilename FALSE 9 TRUE FALSE FALSE TRUE TRUE)
             )
-            ;file-png-save parameters
-              ;The run mode { RUN-INTERACTIVE (0), RUN-NONINTERACTIVE (1) }
-              ;Input image
-              ;Drawable to save
-              ;filename
-              ;raw-filename - this doesn't appear to do anything
-              ;Adam7 interlacing(Interlacing) Checking interlace allows an image on a web page to be progressively displayed as it is downloaded. Progressive image display is useful with slow connection speeds, because you can stop an image that is of no interest; interlace is of less use today with our faster connection speeds.
-              ;deflate compression factor (0-9) Since compression is not lossy, the only reason to use a compression level less than 9, is if it takes too long to compress a file on a slow computer. Nothing to fear from decompression: it is as quick whatever the compression level.
-              ;Write bKGD chunk(save background color) If your image has many transparency levels, the Internet browsers that recognize only two levels, will use the background color of your Toolbox instead. Internet Explorer up to version 6 did not use this information.
-              ;Write gAMMA chunk(Save gamma) Gamma correction is the ability to correct for differences in how computers interpret color values. This saves gamma information in the PNG that reflects the current Gamma factor for your display. Viewers on other computers can then compensate to ensure that the image is not too dark or too bright.
-              ;Write oFFs chunk(Save layer offset) PNG supports an offset value called the “oFFs chunk”, which provides position data. Unfortunately, PNG offset support in GIMP is broken, or at least is not compatible with other applications, and has been for a long time. Do not enable offsets, let GIMP flatten the layers before saving, and you will have no problems.
-              ;Write pHYS chunk(Save Resolution) Save the image resolution, in ppi (pixels per inch). Are the pHYS and tIME parameters swapped in DB Browser?(from http://beefchunk.com/documentation/lang/gimp/GIMP-Scripts-Fu.html)
-              ;Write tIME chunk(Save Creation Time) Date the file was saved.
-            (file-png-save RUN-NONINTERACTIVE image (car (gimp-image-get-active-drawable image)) outputFilename outputFilename FALSE 9 TRUE FALSE FALSE TRUE TRUE)
           )
+          ((= outputType 1)
 
-          (let*
-            (
-              (outputFilename (string-append outputFilenameNoExtension ".jpg"))  ;add the new extension
+            (let*
+              (
+                (outputFilename (string-append outputFilenameNoExtension ".jpg"))  ;add the new extension
+              )
+              ;file-jpeg-save parameters
+                ;The run mode(RUN-INTERACTIVE(0), RUN-NONINTERACTIVE(1))
+                ;Input image
+                ;Drawable to save
+                ;filename
+                ;raw-filename - this doesn't appear to do anything
+                ;quality(0-1)
+                ;smoothing(0-1)
+                ;use optimized tables during huffman encoding(TRUE/FALSE)
+                ;create progressive JPEG images(TRUE/FALSE)
+                ;image comment(string)
+                ;Sub-sampling type { 0, 1, 2, 3 } 0 == 4:2:0 (chroma quartered), 1 == 4:2:2 Horizontal (chroma halved), 2 == 4:4:4 (best quality), 3 == 4:2:2 Vertical (chroma halved)
+                ;Force creation of a baseline JPEG (non-baseline JPEGs can't be read by all decoders) (TRUE/FALSE)
+                ;Interval of restart markers (in MCU rows, 0 = no restart markers)
+                ;DCT method to use { INTEGER (0), FIXED (1), FLOAT (2) }
+              (file-jpeg-save RUN-NONINTERACTIVE image (car (gimp-image-get-active-drawable image)) outputFilename outputFilename (/ outputQuality 100) 0 TRUE TRUE "" 2 TRUE 0 0)
             )
-            ;file-jpeg-save parameters
-              ;The run mode(RUN-INTERACTIVE(0), RUN-NONINTERACTIVE(1))
-              ;Input image
-              ;Drawable to save
-              ;filename
-              ;raw-filename - this doesn't appear to do anything
-              ;quality(0-1)
-              ;smoothing(0-1)
-              ;use optimized tables during huffman encoding(TRUE/FALSE)
-              ;create progressive JPEG images(TRUE/FALSE)
-              ;image comment(string)
-              ;Sub-sampling type { 0, 1, 2, 3 } 0 == 4:2:0 (chroma quartered), 1 == 4:2:2 Horizontal (chroma halved), 2 == 4:4:4 (best quality), 3 == 4:2:2 Vertical (chroma halved)
-              ;Force creation of a baseline JPEG (non-baseline JPEGs can't be read by all decoders) (TRUE/FALSE)
-              ;Interval of restart markers (in MCU rows, 0 = no restart markers)
-              ;DCT method to use { INTEGER (0), FIXED (1), FLOAT (2) }
-            (file-jpeg-save RUN-NONINTERACTIVE image (car (gimp-image-get-active-drawable image)) outputFilename outputFilename (/ outputQuality 100) 0 TRUE TRUE "" 2 TRUE 0 0)
+          )
+          (else
+            (let*
+              (
+                (outputFilename (string-append outputFilenameNoExtension ".gif"))  ;add the new extension
+              )
+              (gimp-image-convert-indexed image 1 0 256 TRUE TRUE "")
+              ;file-gif-save parameters
+                ;The run mode(RUN-INTERACTIVE(0), RUN-NONINTERACTIVE(1))
+                ;Input image
+                ;Drawable to save
+                ;filename
+                ;raw-filename - this doesn't appear to do anything
+                ;Try to save as interlaced(TRUE/FALSE?)
+                ;(animated gif) loop infinitely(TRUE/FALSE?)
+                ;(animated gif) Default delay between frames in milliseconds
+                ;(animated gif) Default disposal type (0=`don't care`, 1=combine, 2=replace)
+              (file-gif-save RUN-NONINTERACTIVE image (car (gimp-image-get-active-drawable image)) outputFilename outputFilename FALSE FALSE 0 0)
+            )
           )
         )
       )
@@ -121,7 +144,7 @@
   ""  ;image type
   SF-DIRNAME  "Source Folder" ""  ;sourcePath
   SF-DIRNAME  "Destination Folder" ""  ;destinationPath
-  SF-OPTION  "Output Type" '("JPEG" "PNG")  ;outputType
+  SF-OPTION  "Output Type" '("PNG" "JPEG" "GIF")  ;outputType
   SF-VALUE  "Output Quality(JPEG only) 0-100" "90"  ;outputQuality
   SF-VALUE  "Max Height" "1500"  ;maxHeight
   SF-VALUE  "Max Width" "1500"  ;maxWidth
